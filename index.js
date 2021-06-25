be9c9eb36ea036b561ed067170d429e54076ec4c
Mahe Karim <mahekarim@deligram.com>
2021-06-26 01:10:42 +0600

    Refresh Button Added

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+	/**
+	 * 	What I Learn From This Project
+	 * 	1. Script tag
+	 * 	2. Variables
+	 * 	3. Numbers
+	 * 	4. Strings
+	 * 	5. console.log
+	 * 	6. functions
+	 * 	7. The DOM
+	 */
+
 	 let countEl = document.getElementById("count-el")
 	 let count = 0
 	 
@@ -9,5 +20,10 @@
 	 let saveEl = document.getElementById("save-el")
 	 function save() {
 		 let saveString = count + "  -  "
-		 saveEl.innerText += saveString;
+		 saveEl.textContent += saveString;
+	 }
+	 
+	 function refresh() {
+		 countEl.textContent = 0
+		 count = 0
 	 }
